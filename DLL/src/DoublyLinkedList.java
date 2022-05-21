dd93aa31e53419c2e38d2bf7366f3a3f4313f50b
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-21 16:30:00 +0530

    Update DoublyLinkedList.java

--- DLL/src/DoublyLinkedList.java
+++ DLL/src/DoublyLinkedList.java
@@ -11,7 +11,7 @@ public class DoublyLinkedList {
         return size==0;
     }
 
-    public void insertAtStart() {
+    public void insertAtStart(int data) {
         Node current = head;
         head = new Node()
     }
