40d8e247388ca07fdc59b83666253da298d84237
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-21 16:17:35 +0530

    Update Node.java

--- DLL/src/Node.java
+++ DLL/src/Node.java
@@ -1,23 +1,19 @@
-public class Node
-{
-	int data;
-	Node previous;
-	Node next;
-	
-	Node(int data)
-	{
-		this.data = data;
-	}
-	
-	Node(Node previous, int data, Node next)
-	{
-		this.previous = previous;
-		this.data = data;
-		this.next = next;
-	}
-	
-	public int data()
-	{
-		return data;
-	}
+public class Node {
+    int data;
+    Node previous;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+
+    Node(Node previous, int data, Node next) {
+        this.previous = previous;
+        this.data = data;
+        this.next = next;
+    }
+
+    public int data() {
+        return data;
+    }
 }
