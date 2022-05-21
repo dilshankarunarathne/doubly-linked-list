df40bcc2fd5d8cac24d0c6705443f1085d775087
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-21 16:34:59 +0530

    Update DoublyLinkedList.java

--- DLL/src/DoublyLinkedList.java
+++ DLL/src/DoublyLinkedList.java
@@ -21,6 +21,7 @@ public class DoublyLinkedList {
     public void insertLast(int data) {
         if (isEmpty()) {
             insertAtStart(data);
+            
         }
         Node lastNode = null;
         while (true) {
