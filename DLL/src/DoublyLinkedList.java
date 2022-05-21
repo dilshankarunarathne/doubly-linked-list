3dbc5a77eb44cc4fd60ba02ece87fed3ad7fb843
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-21 16:22:44 +0530

    Update DoublyLinkedList.java

--- DLL/src/DoublyLinkedList.java
+++ DLL/src/DoublyLinkedList.java
@@ -45,7 +45,7 @@ public class DoublyLinkedList {
 			System.out.println("List is empty");
 			return;
 		}
-		System.out.println("Nodes of doubly linked list: ");
+		System.out.println(" ");
 		while(current != null) {
 			System.out.print(current.data + " ");
 			current = current.next;
