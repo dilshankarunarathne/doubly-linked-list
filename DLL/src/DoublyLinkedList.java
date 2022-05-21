753701b5640d80e8c7739a0bcf4d1ed7c1d53dec
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-21 16:17:58 +0530

    Update DoublyLinkedList.java

--- DLL/src/DoublyLinkedList.java
+++ DLL/src/DoublyLinkedList.java
@@ -1,56 +1,45 @@
-public class DoublyLinkedList
-{
-	Node head;
-	int size;
-	
-	public DoublyLinkedList()
-	{
-		head = null;
-		size = 0;
-	}
-	
-	public boolean isEmpty()
-	{
-		// Code goes here...
-	}
-	
-	public void insertAtStart()
-	{
-		// Code goes here...
-	}
-	
-	public void insertLast()
-	{
-		// Code goes here...
-	}
-	
-	public void insertAt()
-	{
-		// Code goes here...
-	}
-	
-	public void deleteFirst()
-	{
-		// Code goes here...
-	}
-	
-	public void deleteLast()
-	{
-		// Code goes here...
-	}
-	
-	public void deleteAt()
-	{
-		// Code goes here...
-	}
-	
-	public void countNodes()
-	{
-		// Code goes here...
-	}
-	
-	public void show()
-	{
-		// Code goes here...
-	}
+public class DoublyLinkedList {
+    Node head;
+    int size;
+
+    public DoublyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        // Code goes here...
+    }
+
+    public void insertAtStart() {
+        // Code goes here...
+    }
+
+    public void insertLast() {
+        // Code goes here...
+    }
+
+    public void insertAt() {
+        // Code goes here...
+    }
+
+    public void deleteFirst() {
+        // Code goes here...
+    }
+
+    public void deleteLast() {
+        // Code goes here...
+    }
+
+    public void deleteAt() {
+        // Code goes here...
+    }
+
+    public void countNodes() {
+        // Code goes here...
+    }
+
+    public void show() {
+        // Code goes here...
+    }
 }
