bfde7e2b5a992d151c5e719dde9affa94be7525a
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-22 11:49:34 +0530

    Update DoublyLinkedList.java

--- DLL/src/DoublyLinkedList.java
+++ DLL/src/DoublyLinkedList.java
@@ -42,7 +42,6 @@ public class DoublyLinkedList {
         }
         lastNode.next = new Node(data);
         lastNode.next.previous = lastNode;
-        size++;
     }
 
     public void insertAt(int data, int position) {
