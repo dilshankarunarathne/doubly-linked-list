2fdb9efa403fb67648b43f990a2e4cf7619e64eb
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-22 12:04:04 +0530

    Update DoublyLinkedList.java

--- DLL/src/DoublyLinkedList.java
+++ DLL/src/DoublyLinkedList.java
@@ -100,7 +100,7 @@ public class DoublyLinkedList {
 
     public void deleteAt(int position) {
         if (position<0) {
-//          // do nothing!
+            // do nothing!
             return;
         }
         if (position>size) {
