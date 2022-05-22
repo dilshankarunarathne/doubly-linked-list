2d985d9f266b9213703108ddc8fe04e6f8e937ac
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-22 11:58:38 +0530

    Update Main.java

--- DLL/src/Main.java
+++ DLL/src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         list.insertAt(21, 1);
         list.insertAt(27, 3);
         list.insertLast(56);
-        list.insertAt(27, -1);
+        list.insertAt(27, -1); 
         list.show();        // output: 11 21 9 27 7 5 4 3 1 2 56
         list.countNodes();    // output: 11
 
