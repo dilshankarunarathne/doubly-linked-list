fb607a4318336970f0e339185f9c037ce84aaae4
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-22 11:41:39 +0530

    Update DoublyLinkedList.java

--- DLL/src/DoublyLinkedList.java
+++ DLL/src/DoublyLinkedList.java
@@ -37,7 +37,7 @@ public class DoublyLinkedList {
             if (lastNode.next == null) {
                 break;
             }
-            lastNode = 
+            lastNode = lastNode.next;
         }
         lastNode.next = new Node(data);
         lastNode.next.previous = lastNode;
