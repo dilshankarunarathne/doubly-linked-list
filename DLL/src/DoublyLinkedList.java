6302ddfad601708455d3d8727b2361bce6a1cdd1
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-22 11:31:40 +0530

    Update DoublyLinkedList.java

--- DLL/src/DoublyLinkedList.java
+++ DLL/src/DoublyLinkedList.java
@@ -89,7 +89,7 @@ public class DoublyLinkedList {
         while (true) {
             if (currentPos == position) {
                 nodeAtPos.previous.next = nodeAtPos.next;
-                nodeAtPos.next.previous = 
+                nodeAtPos.next.previous = nodeAtPos.previous;
                 return;
             }
             nodeAtPos = nodeAtPos.next;
