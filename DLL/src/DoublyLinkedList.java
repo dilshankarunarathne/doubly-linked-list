3ca60fa17aa7c0a3d9ede46b03c9333efc783436
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-05-22 11:30:55 +0530

    ws

--- DLL/src/DoublyLinkedList.java
+++ DLL/src/DoublyLinkedList.java
@@ -88,7 +88,7 @@ public class DoublyLinkedList {
         int currentPos = 1;
         while (true) {
             if (currentPos == position) {
-                
+                nodeAtPos
                 return;
             }
             nodeAtPos = nodeAtPos.next;
